f9a7bfc74d4bb84260259c1c4169dc4f283df73a
moxuy <jichangee@gmail.com>
2022-07-05 23:05:58 +0800

    update

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     let mut s = String::from("hello world!");
     let word = first_word(&s);
-    s.clear();
     println!("word: {}", word);
+    s.clear();
 }
 
 fn first_word(s: &str) -> &str {
